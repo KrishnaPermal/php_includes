7dcde1514fe0a378313336950ba72fdc2e9216c8
KrishnaPermal <krishna.tantalidespermalnaiken@gmail.com>
2019-10-01 13:27:33 +0400

    second commit

---
+++ footer.php
@@ -0,0 +1,41 @@
+<footer class="bg-light">
+
+        <div class="container">
+        <div class="row">
+            <div class="card  border-0 bg-transparent" style="width: 18rem;">
+                <div class="card-body">
+                    <h1 class="card-title">Introuvable</h1>
+                    <p class="card-text">Nous sommes désolés, nous n'avons pas pu trouver ce que vous recherchiez.</p>
+                    <p class="card-text">Vous pouvez effectuer une recherche ou revenir à la <a href="">page
+                            d'accueil</a>.</p>
+                    <p class="card-text">Si vous suiviez un lien depuis un document existant, vous pouvez vous connecter
+                        et créer la page. Autrement, veuillez <a href="">rapportez un bug.</a> Merci !</p>
+                </div>
+            </div>
+
+            <div class="card  border-0 bg-transparent" style="width: 18rem;">
+                <div class="card-body">
+                    <h1 class="card-title">MDN</h1>
+                    <p class="card-text">Nous sommes désolés, nous n'avons pas pu trouver ce que vous recherchiez.</p>
+                    <p class="card-text">Vous pouvez effectuer une recherche ou revenir à la <a href="">page
+                            d'accueil</a>.</p>
+                    <p class="card-text">Si vous suiviez un lien depuis un document existant, vous pouvez vous connecter
+                        et créer la page. Autrement, veuillez <a href="">rapportez un bug.</a> Merci !</p>
+                </div>
+            </div>
+
+
+            <div class="card  border-0 bg-transparent" style="width: 18rem;">
+                <div class="card-body">
+                    <h1 class="card-title">Introuvable</h1>
+                    <p class="card-text">Nous sommes désolés, nous n'avons pas pu trouver ce que vous recherchiez.</p>
+                    <p class="card-text">Vous pouvez effectuer une recherche ou revenir à la <a href="">page
+                            d'accueil</a>.</p>
+                    <p class="card-text">Si vous suiviez un lien depuis un document existant, vous pouvez vous connecter
+                        et créer la page. Autrement, veuillez <a href="">rapportez un bug.</a> Merci !</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    </footer>
